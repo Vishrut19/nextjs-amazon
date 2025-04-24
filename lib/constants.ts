cbbee9037e701ee367b181e8d7c20d3df1a15217
rsrohitsingh511 <rsrohitsingh511@gmail.com>
2025-04-24 19:05:30 +0530

    Added Product Details Page

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,3 +4,6 @@ export const APP_SLOGAN =
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   "An Amazon clone built with Next.js and MongoDB";
+
+// We will show 9 items in Result Page
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9);
